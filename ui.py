afb69b03ef99ef8862c2a0eaa73de8481f242e48
Nazar Pasichnyk <nazarpas2007@gmail.com>
2024-11-23 02:28:14 +0200

    feat(ui.py): add build ui logic and demo preview

--- ui.py
+++ ui.py
@@ -1,10 +1,14 @@
 """ UI module with main program loop """
 
-from graph_logic import divide_dishes_by_type 
 import curses
 
-UI_WIDTH = 40
-UI_HEIGHT = 20
+from ui_tools.matrix import (Matrix, create_matrix, fill_matrix,
+                             draw_matrix_on_screen, draw_matrix_on_matrix)
+from ui_tools.ui_toolbox import create_text_field
+from graph_logic import divide_dishes_by_type
+
+UI_WIDTH = 60
+UI_HEIGHT = 30
 
 # Food data for testing
 TESTING_FOOD_DATA = (
@@ -13,7 +17,7 @@ TESTING_FOOD_DATA = (
     ["компот", "сік апельсиновий", "сік яблучний"]
     )
 
-def handle_terminal_size(stdscr: curses.window):
+def handle_terminal_size(stdscr: curses.window) -> bool:
     """ Check if terminal is big enough. If it isnt, ask user to resize it """
     # Dont start app until terminal big enough
     while True:
@@ -28,10 +32,15 @@ def handle_terminal_size(stdscr: curses.window):
             stdscr.addstr(3, 0, "Resize the terminal.")
             stdscr.refresh()
 
-            # Wait a little bit before rechecking size
-            stdscr.getch()
+            key = stdscr.getch()
+            # ESC key code - 27
+            if key in [ord("q"), ord("Q"), 27]:
+                # Return ui_runs = False if q is pressed
+                return False
         else:
             break  # Terminal is large enough
+
+    return True
 
 def init_curses() -> curses.window:
     """ Init curses library and check if terminal is big enogh
@@ -50,6 +59,9 @@ def init_curses() -> curses.window:
     # So "key = stdscr.getch()" wont stop program like input(), but wil wait for 100ms
     stdscr.timeout(500)
 
+    # Check if terminal size is okay for start
+    handle_terminal_size(stdscr)
+
     return stdscr
 
 def main_loop(testing_mode: bool=False):
@@ -58,17 +70,19 @@ def main_loop(testing_mode: bool=False):
 
     # Flag that indicate if program should still run
     ui_runs = True
+    # Terminal screen matrix
+    screen_matrix = create_matrix(UI_WIDTH, UI_HEIGHT)
     # Variable to hold food values for each food row
     typed_food = ([], [], [])
 
     while ui_runs:
-        handle_terminal_size(stdscr)
         if testing_mode:
             typed_food = TESTING_FOOD_DATA
         else:
             raise NotImplementedError()
-        draw_ui(stdscr, typed_food)
-        ui_runs = handle_keys_input(stdscr, ui_runs)
+        build_ui(screen_matrix, typed_food)
+        draw_matrix_on_screen(stdscr, screen_matrix)
+        ui_runs = handle_keys_input(stdscr, ui_runs) and handle_terminal_size(stdscr)
 
 def handle_keys_input(stdscr: curses.window, ui_runs: bool) -> bool:
     """ Handle keys input"""
@@ -79,15 +93,15 @@ def handle_keys_input(stdscr: curses.window, ui_runs: bool) -> bool:
 
     return ui_runs
 
-def draw_ui(stdscr: curses.window, typed_food: tuple[list[str], list[str], list[str]]):
-    """ Draw UI """
-    height, width = stdscr.getmaxyx()
-    stdscr.clear()
-    stdscr.addstr(2, 0, f"Current size: {width}x{height}")
-    stdscr.addstr(3, 0, ", ".join(typed_food[0]))
-    stdscr.addstr(4, 0, ", ".join(typed_food[1]))
-    stdscr.addstr(5, 0, ", ".join(typed_food[2]))
-    stdscr.refresh()
+def build_ui(screen_matrix: Matrix, typed_food: tuple[list[str], list[str], list[str]]
+             ) -> Matrix:
+    """ Build UI screen matrix """
+    fill_matrix(screen_matrix, " ")
+    for row in range(3):
+        for column in range(min(3, len(typed_food[row]))):
+            dish_frame = create_text_field(typed_food[row][column], 17, 7)
+            draw_matrix_on_matrix(dish_frame, screen_matrix, column*20, row*8)
+
 
 if __name__ == "__main__":
     main_loop(testing_mode=True)
