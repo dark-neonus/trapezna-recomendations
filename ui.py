d4a2d7d9686c1a15a6924034f8d16a21a9444610
Nazar Pasichnyk <nazarpas2007@gmail.com>
2024-12-03 21:25:08 +0200

    feat(ui.py): add 'visualization' and 'clear graph' buttons

--- ui.py
+++ ui.py
@@ -96,7 +96,6 @@ def main_loop(testing_mode: bool=False):
                 dishes_dx
                 )
         draw_matrix_on_screen(stdscr, screen_matrix)
-        # ui_runs = handle_keys_input(stdscr, ui_runs) and handle_terminal_size(stdscr)
 
         # Handle key input
         key = stdscr.getch()
@@ -124,13 +123,16 @@ def main_loop(testing_mode: bool=False):
                 min(cursor_position[1] + 1, len(typed_dishes) - 1)
                 )
         elif key in [curses.KEY_ENTER, ord("\n"), ord(" ")]:
-            if cursor_position == (0, 3):
+            if cursor_position == (0, 3): # Buy button
                 typed_dishes = get_sorted_dishes(selected_dishes,
                                                  typed_dishes,
                                                  testing_mode)
                 selected_dishes = []
                 cursor_position = (0, 0)
-
+            elif cursor_position == (1, 3): # Visualization button
+                show_visualization()
+            elif cursor_position == (2, 3): # Clear graph button
+                clear_graph()
             elif cursor_position in selected_dishes:
                 selected_dishes.remove(cursor_position)
             else:
@@ -160,10 +162,18 @@ def build_ui(screen_matrix: Matrix, typed_food: tuple[list[str], list[str], list
                 frame_symbol = "█"
             elif (column + dishes_dx[row], row) in selected_dishes:
                 frame_symbol = "░"
-            dish_frame = create_text_field(
-                f"{column + dishes_dx[row]}) {typed_food_row[column + dishes_dx[row]]}",
-                DISH_BLOCK_WIDTH, [3, 5, 7, 5, 3][column], frame_symbol
-                )
+            elif row == 3:
+                frame_symbol = " "
+            if row == 3:
+                dish_frame = create_text_field(
+                    f"{typed_food_row[column + dishes_dx[row]]}",
+                    DISH_BLOCK_WIDTH, 3, frame_symbol
+                    )
+            else:
+                dish_frame = create_text_field(
+                    f"{column + dishes_dx[row] + 1}) {typed_food_row[column + dishes_dx[row]]}",
+                    DISH_BLOCK_WIDTH, [3, 5, 7, 5, 3][column], frame_symbol
+                    )
             draw_matrix_on_matrix(dish_frame, screen_matrix,
                                 column*(DISH_BLOCK_WIDTH+2) + 2, row*8 +2)
     add_frame_to_matrix(screen_matrix)
@@ -172,9 +182,13 @@ def get_sorted_dishes(selected_dishes: list[str],
                typed_dishes: list[list[str], list[str], list[str]],
                testing_mode: bool = False
                ) -> list[list[str], list[str], list[str]]:
+    """ Return list of sorted dishes with buttons """
+    # Buttons row
+    buttons_row = [["Купити", "Візуалізація", "! Обнули !"]]
+
     # Case when nothing was selected dont change anything
     if testing_mode:
-        return TESTING_FOOD_DATA + [["Купити"]]
+        return TESTING_FOOD_DATA + buttons_row
 
     graph = graph_logic.load_graph(GRAPH_FILE)
 
@@ -198,7 +212,21 @@ def get_sorted_dishes(selected_dishes: list[str],
         graph_logic.sort_dishes(dishes_power)
     )
 
-    return list(new_typed_dishes) + [["Купити"]]
+    return list(new_typed_dishes) + buttons_row
+
+def show_visualization():
+    """ Show visualization of current graph
+    by calling visualization part of program
+    """
+    pass
+
+def clear_graph():
+    """ Clear graph with preferences
+    """
+    # Write mode create/overwrite file
+    with open("graph.json", "w", encoding="utf-8") as file:
+        # Write empty dictionary to `graph.json`
+        file.write("{\n}")
 
 if __name__ == "__main__":
     main_loop(testing_mode=True)
